1ebd10470a4ea816965aa70f6af2d48f39d8c5f3
mehdiknn <mehdi@kannounnel.dev>
2022-05-04 15:50:29 +0200

    Init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="text-3xl">
+    <div class=" text-3xl">{{ msg }}</div>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
